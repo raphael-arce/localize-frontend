4467ffa9dce11c0c8faecea7571c4162a7eb3475
Raphael Arce <raphael.arce@hotmail.com>
2022-09-10 22:41:26 +0200

    replaced favicon, added no results card, added loading button

--- src/components/app/searchForm.tsx
+++ src/components/app/searchForm.tsx
@@ -13,11 +13,15 @@ export const SearchForm = component$(() => {
       storeAddresses: {} as StoreAdresses
     } as SearchResult,
     currentlySelected: 0,
+    isSearching: false
   });
 
   useClientEffect$(async () => {
     state.query = getQuery();
+
+    state.isSearching = true;
     state.searchResult = await search(state.query);
+    state.isSearching = false;
 
     if (!state.searchResult.products[0]) {
       return;
@@ -39,7 +43,9 @@ export const SearchForm = component$(() => {
             state.currentlySelected = 0;
             removeMarkers();
 
+            state.isSearching = true;
             state.searchResult = await search(state.query);
+            state.isSearching = false;
 
             if (!state.searchResult.products[0]) {
               return;
@@ -51,43 +57,51 @@ export const SearchForm = component$(() => {
             value={state.query}
             name="q"
             id="searchInput"
-            class="input input-bordered w-full max-w-xs"
+            class={`input input-bordered w-full max-w-xs ${!state.isSearching && !state.searchResult.products[0] && state.query ? 'input-error' : ''}`}
             placeholder="Search something!"
           />
-          <button class="btn ml-1">Search</button>
+          <button class={`btn ml-1 ${state.isSearching ? 'loading' : ''}`} disabled={state.isSearching}>{ state.isSearching ? 'Loading' : 'Search' }</button>
         </form>
+        { !state.isSearching && !state.searchResult.products[0] && state.query !== '' ?
+        <div className="card w-60 bg-base-100 shadow-xl m-2">
+          <div className="card-body">
+            <h2 className="card-title">Woopsie!</h2>
+            <p>No results for "{state.query}" were found </p>
+          </div>
+        </div> : null
+        }
         <div class="w-screen h-1/3 overflow-auto pt-1">
           <div class="flex flex-nowrap">
-              {
-                state.searchResult.products.map(({ title, availableAt, price, imageUrlTemplates }, index) => {
-                  const imageUrl = imageUrlTemplates[0].replace('{transformations}', 'f_auto,q_auto,c_fit,h_270,w_260');
+            {
+              state.searchResult.products.map(({ title, availableAt, price, imageUrlTemplates }, index) => {
+                const imageUrl = imageUrlTemplates[0].replace('{transformations}', 'f_auto,q_auto,c_fit,h_270,w_260');
 
-                  return (
-                    <div
-                      class={`flex-none card w-60 bg-base-100 shadow-xl ml-2 mb-2 ${state.currentlySelected === index ? 'ring ring-blue-500' : ''}`}
-                      onClick$={() => {
-                        removeMarkers();
+                return (
+                  <div
+                    class={`flex-none card w-60 bg-base-100 shadow-xl ml-2 mb-2 ${state.currentlySelected === index ? 'ring ring-blue-500' : ''}`}
+                    onClick$={() => {
+                      removeMarkers();
 
-                        if (index !== state.currentlySelected) {
-                          setMarkers(availableAt, state.searchResult.storeAddresses);
-                          state.currentlySelected = index;
-                          return;
-                        }
+                      if (index !== state.currentlySelected) {
+                        setMarkers(availableAt, state.searchResult.storeAddresses);
+                        state.currentlySelected = index;
+                        return;
+                      }
 
-                        state.currentlySelected = -1;
-                      }}
-                    >
-                      <figure class="h-40 px-5 pt-5">
-                        <img class="w-auto h-full" loading="lazy" src={imageUrl} alt="alt" />
-                      </figure>
-                      <div class="card-body h-full">
-                        <h2 class="card-title">{price}</h2>
-                        <p>{title}</p>
-                      </div>
+                      state.currentlySelected = -1;
+                    }}
+                  >
+                    <figure class="h-40 px-5 pt-5">
+                      <img class="w-auto h-full" loading="lazy" src={imageUrl} alt="alt" />
+                    </figure>
+                    <div class="card-body h-full">
+                      <h2 class="card-title">{price}</h2>
+                      <p>{title}</p>
                     </div>
-                  );
-                })
-              }
+                  </div>
+                );
+              })
+            }
           </div>
         </div>
       </div>
