c8e03f4acb5f36f2732fb2e2fc3c24bd78aca8e3
Raphael Arce <raphael.arce@hotmail.com>
2022-09-12 22:03:22 +0200

    added conditional backed URL (development / production)

--- src/components/app/utils/searchUtils.tsx
+++ src/components/app/utils/searchUtils.tsx
@@ -2,8 +2,9 @@ import { Product, SearchResult, StoreAdresses } from '../interfaces';
 
 export async function search(query: string): Promise<SearchResult> {
     if (query !== '') {
+        const url = import.meta.env.DEV ? 'http://127.0.0.1:3000/hello' : 'https://j6obqvfpnwiomxcpbu6hu22bw40zzlke.lambda-url.eu-central-1.on.aws/';
         const response = await fetch(
-          `https://j6obqvfpnwiomxcpbu6hu22bw40zzlke.lambda-url.eu-central-1.on.aws/?q=${encodeURIComponent(query)}`,
+          `${url}?q=${encodeURIComponent(query)}`,
           { mode: 'cors' });
         return response.json();
     }
