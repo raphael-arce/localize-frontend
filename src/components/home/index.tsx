7a446c2e1c752c46aa0f140b9521da67f152c3af
Raphael Arce <raphael.arce@hotmail.com>
2022-09-18 22:18:05 +0200

    added light and dark versions of logo svgs, and replaced the logo img element with picture

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -3,15 +3,19 @@ import {component$} from "@builder.io/qwik";
 export default component$(() => {
 
   return <>
-    <div class="flex h-screen w-screen justify-center flex-col">
+    <div class="flex flex-col h-screen w-screen justify-center" style={{maxHeight: '-webkit-fill-available', maxWidth: '-webkit-fill-available'}}>
       <div class="flex justify-center items-center gap-2 m-2">
-        <img
-          alt="Qwik Logo"
-          width={100}
-          height={37}
-          loading="lazy"
-          src="logo.png"
-        />
+        <picture>
+          <source srcSet="logo.svg" media="(prefers-color-scheme: light)" />
+          <source srcSet="logo-dark.svg" media="(prefers-color-scheme: dark)" />
+          <img
+            alt="Localize Logo"
+            width={100}
+            height={37}
+            loading="lazy"
+            src="logo.png"
+          />
+        </picture>
         <h1 class="text-4xl font-bold">
           Localize
         </h1>
