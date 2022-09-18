087a08b467a7e5027e48b9f61d6b699d0a25c3c5
Raphael Arce <raphael.arce@hotmail.com>
2022-09-18 21:10:39 +0200

    removed unused imports

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,5 @@
 import { component$ } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
-import { App } from "~/components/app/app";
 import Home from "~/components/home";
 
 export default component$(() => {
