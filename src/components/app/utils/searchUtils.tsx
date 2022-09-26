c826c323f85fbcd77042a4647e8705448df8943a
Raphael Arce <raphael.arce@hotmail.com>
2022-09-27 01:58:31 +0200

    updated aws url

--- src/components/app/utils/searchUtils.tsx
+++ src/components/app/utils/searchUtils.tsx
@@ -2,7 +2,7 @@ import { Product, SearchResult, StoreAdresses } from '../interfaces';
 
 export async function search(query: string): Promise<SearchResult> {
     if (query !== '') {
-        const url = import.meta.env.DEV ? 'http://127.0.0.1:3000/search' : 'https://j6obqvfpnwiomxcpbu6hu22bw40zzlke.lambda-url.eu-central-1.on.aws/';
+        const url = import.meta.env.DEV ? 'http://127.0.0.1:3000/search' : 'https://aziofzlwon2st7w4wlwl2ipxr40wywui.lambda-url.eu-central-1.on.aws/ ';
         const response = await fetch(
           `${url}?q=${encodeURIComponent(query)}`,
           { mode: 'cors' });
