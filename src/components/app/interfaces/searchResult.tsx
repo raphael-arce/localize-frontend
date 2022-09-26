513b14706bf1a3d9fafd15e733a542f02e2bfb61
Raphael Arce <raphael.arce@hotmail.com>
2022-09-26 08:34:17 +0200

    updated interfaces to show priceRange and removed unecessary types

--- src/components/app/interfaces/searchResult.tsx
+++ src/components/app/interfaces/searchResult.tsx
@@ -23,23 +23,3 @@ export interface SearchResult {
   products: Product[],
   storeAddresses: StoreAdresses
 }
-
-// export interface SearchResult {
-//     count: number,
-//     currentPage: number,
-//     facets: {
-//             key: string,
-//             multiSelect: boolean,
-//             operation: string,
-//             order: number,
-//             sort: string,
-//             translation: string,
-//             values: {
-//               name: string,
-//               count: number
-//             }[]
-//     }[],
-//     pageSize: number,
-//     products: Product[],
-//     totalPages: number,
-// }
