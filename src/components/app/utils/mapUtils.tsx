a68a2323807eeedaef78b6bd5515aa99319cfec2
Raphael Arce <raphael.arce@hotmail.com>
2022-09-27 00:20:06 +0200

    added new custom marker

--- src/components/app/utils/mapUtils.tsx
+++ src/components/app/utils/mapUtils.tsx
@@ -1,34 +1,72 @@
 import { AvailableAt, StoreAdresses } from '../interfaces';
 import mapboxgl, { Marker, Map } from 'mapbox-gl';
 
+function htmlToElement(html: string): HTMLElement {
+  const template = document.createElement('template');
+  html = html.trim(); // Never return a text node of whitespace as the result
+  template.innerHTML = html;
+  return template.content.firstChild as HTMLElement;
+}
+
 declare var myMap: Map;
 declare var currentMarkers: Marker[];
 
-const setMarkers = (price: string, availableAt: AvailableAt[] | undefined, storeAddresses: StoreAdresses) => {
+const setMarkers = (availableAt: AvailableAt[] | undefined, storeAddresses: StoreAdresses) => {
   if (!availableAt) {
     return;
   }
 
-  for (const { storeId, stockLevel } of availableAt) {
-    if (!storeAddresses[storeId] || stockLevel === 0) {
+  for (const { storeId, inStock, stockLevel, formattedPrice } of availableAt) {
+    if (!storeAddresses[storeId] || !inStock) {
       continue;
     }
 
+
+
     const { lon, lat } = storeAddresses[storeId].location;
     const { address } = storeAddresses[storeId];
+    const customMarker = htmlToElement(`
+<div class="marker" style="position: absolute;">
+    <div style="
+            position: relative;
+            display: inline-flex;
+            flex-direction: column;
+            align-items: center;
+        ">
+        <div style="
+            border-radius: 9999px;
+            border-width: 1px;
+            border: black;
+            background: white;
+            display: inline-flex;
+            padding-inline: 10px;
+            padding-block: 3px;"
+        >
+            ${formattedPrice}
+        </div>
+        <div style="
+                border-left: 10px solid transparent;
+                border-right: 10px solid transparent;
+                border-top: 10px solid #fff;
+                flex: none;
+                width: 2px;
+            "/>
+    </div>
+</div>
+    `);
     const marker = new mapboxgl
-      .Marker()
-      .setLngLat([lon, lat])
-      .setPopup(new mapboxgl.Popup().setHTML(`
+      .Marker({ element: customMarker, anchor: 'bottom' })
+      .setLngLat([ lon, lat ])
+      .setPopup(new mapboxgl.Popup({offset: 40}).setHTML(`
         <h2>${address.name}</h2>
         <p>
             ${address.street}
             <br>
             ${address.zip}, ${address.city}
             <br>
-            <span class="text-green-800"><b>${stockLevel}</b> Verfügbar</span>
+            <span class="text-green-800"><b>${stockLevel ?? ''}</b> Verfügbar</span>
         </p>
-      `)) // add popup
+      `))
       .addTo(myMap);
     currentMarkers.push(marker);
   }
