4faaffa2aaf6018f29ab817e0fb269e4146c88f9
Raphael Arce <raphael.arce@hotmail.com>
2022-10-03 22:00:55 +0200

    reworked markers and added dark mode support for the map

--- src/components/app/utils/mapUtils.tsx
+++ src/components/app/utils/mapUtils.tsx
@@ -8,6 +8,18 @@ function htmlToElement(html: string): HTMLElement {
   return template.content.firstChild as HTMLElement;
 }
 
+function getIcon(storeId: string) {
+  const storeName = storeId.includes('DM') ? 'DM' : 'ROSSMANN';
+  switch (storeName) {
+    case 'DM':
+      return 'storeFavicons/DM.png';
+    case 'ROSSMANN':
+      return 'storeFavicons/ROSSMANN.ico';
+    default:
+      return '';
+  }
+}
+
 declare var myMap: Map;
 declare var currentMarkers: Marker[];
 
@@ -21,51 +33,39 @@ const setMarkers = (availableAt: AvailableAt[] | undefined, storeAddresses: Stor
       continue;
     }
 
-
-
     const { lon, lat } = storeAddresses[storeId].location;
     const { address } = storeAddresses[storeId];
     const customMarker = htmlToElement(`
-<div class="marker" style="position: absolute;">
-    <div style="
-            position: relative;
-            display: inline-flex;
-            flex-direction: column;
-            align-items: center;
-        ">
-        <div style="
-            border-radius: 9999px;
-            border-width: 1px;
-            border: black;
-            background: white;
-            display: inline-flex;
-            padding-inline: 10px;
-            padding-block: 3px;"
-        >
-            ${formattedPrice}
-        </div>
-        <div style="
-                border-left: 10px solid transparent;
-                border-right: 10px solid transparent;
-                border-top: 10px solid #fff;
-                flex: none;
-                width: 2px;
-            "/>
-    </div>
-</div>
+      <div class="marker" style="position: absolute;">
+          <div class="localize-marker">
+            <div class="localize-marker-bubble subpixel-antialiased">
+              <img src="${getIcon(storeId)}" style="height: 16px; margin-right: 5px"/>
+              ${formattedPrice}
+            </div>
+            <div class="localize-marker-pointer">
+              <div class="localize-marker-pointer-left"></div>
+              <div class="localize-marker-pointer-middle">
+                <div class="localize-marker-pointer-middle-1"></div>
+                <div class="localize-marker-pointer-middle-2"></div>
+                <div class="localize-marker-pointer-middle-3"></div>
+              </div>
+              <div class="localize-marker-pointer-right"></div>
+            </div>
+          </div>
+      </div>
     `);
     const marker = new mapboxgl
       .Marker({ element: customMarker, anchor: 'bottom' })
       .setLngLat([ lon, lat ])
       .setPopup(new mapboxgl.Popup({offset: 40}).setHTML(`
-        <h2>${address.name}</h2>
-        <p>
-            ${address.street}
-            <br>
-            ${address.zip}, ${address.city}
-            <br>
-            <span class="text-green-800"><b>${stockLevel ?? ''}</b> Verfügbar</span>
-        </p>
+          <h2>${address.name}</h2>
+          <p>
+              ${address.street}
+              <br>
+              ${address.zip}, ${address.city}
+              <br>
+              <span class="text-green-800"><b>${stockLevel ?? ''}</b> Verfügbar</span>
+          </p>
       `))
       .addTo(myMap);
     currentMarkers.push(marker);
--- src/components/searchMap/searchMap.tsx
+++ src/components/searchMap/searchMap.tsx
@@ -1,16 +1,18 @@
 import { component$, useClientEffect$ } from '@builder.io/qwik';
 import mapboxgl, { Map } from 'mapbox-gl';
 
-declare var myMap: Map;
+declare var myMap: Map, isDarkMode: boolean;
 
 export const SearchMap = component$(() => {
 
     useClientEffect$(() => {
         mapboxgl.accessToken = 'pk.eyJ1IjoibXVsdGlmbHV4IiwiYSI6ImNsNndsaDJmcjA1Zjkzam15bzE3emQwY3AifQ.HzJi-VhL35sbroQR1-nVJA';
 
+        const style = isDarkMode ? 'mapbox://styles/multiflux/cl8oyzobp006y14pfevqtdls0' : 'mapbox://styles/mapbox/streets-v11'
+
         myMap = new mapboxgl.Map({
             container: 'map', // container ID
-            style: 'mapbox://styles/mapbox/streets-v11', // style URL
+            style,
             center: [13.3817493, 52.5114016], // starting position [lng, lat]
             zoom: 12, // starting zoom
             projection: {name: 'globe'} // display the map as a 3D globe
