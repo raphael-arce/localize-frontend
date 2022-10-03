a5004c407e3feb80f411e67b1a0e8a6e31542281
Raphael Arce <raphael.arce@hotmail.com>
2022-10-03 22:15:57 +0200

    added apple touch icon

--- src/components/head/head.tsx
+++ src/components/head/head.tsx
@@ -15,6 +15,7 @@ export const Head = component$(() => {
       <link rel='icon' type="image/svg" sizes="32x32" href="/logo.svg" />
       <link rel='icon' type="image/svg" sizes="32x32" href="/favicon-32x32.png" />
       <link rel='icon' type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <link rel="canonical" href={loc.href} />
 
