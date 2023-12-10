fbaf639a9e55735341738b90a378c06e24d4e03e
Raphael Arce <raphael.arce@ts.berlin>
2023-12-10 17:53:38 +0100

    feat: replaced aws api with cloudflare workers

--- src/components/app/utils/searchUtils.tsx
+++ src/components/app/utils/searchUtils.tsx
@@ -1,8 +1,9 @@
 import { Product, SearchResult, StoreAdresses } from '../interfaces';
 
+const url = import.meta.env.DEV ? 'http://0.0.0.0:8787/' : "https://localize-backend.localize-backend.workers.dev/" ;
+
 export async function search(query: string): Promise<SearchResult> {
     if (query !== '') {
-        const url = import.meta.env.DEV ? 'http://127.0.0.1:3000/search' : 'https://aziofzlwon2st7w4wlwl2ipxr40wywui.lambda-url.eu-central-1.on.aws/ ';
         const response = await fetch(
           `${url}?q=${encodeURIComponent(query)}`,
           { mode: 'cors' });
